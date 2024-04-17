3686049ea8a712e62ae04134d6d86695a94838b1
Az1kk11 <azizbekjaqsilikov@gmail.com>
2024-04-17 17:30:31 +0500

    login

--- src/Redux/services/api.ts
+++ src/Redux/services/api.ts
@@ -5,13 +5,9 @@ axios.defaults.baseURL = 'https://futureskillshub.uz/api'
 
 axios.interceptors.request.use(config => {
 
-    if(!axios.AxiosError.ERR_BAD_REQUEST){
-        const authorization = getItem('token') ? `Bearer ${getItem('token')}` : ''
-        config.headers.Authorization = authorization
-    }else{
         const authorization = getItem('tokenUser') ? `Bearer ${getItem('tokenUser')}` : ''
         config.headers.Authorization = authorization
-    }
+    
     return config
 })
 
